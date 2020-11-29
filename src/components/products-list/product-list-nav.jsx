dea883492b576ff85c7023b28e913a6873039031
Oleg <talrash666@gmail.com>
2020-11-29 16:33:39 +0300

    change product navagation to dropdown

--- src/components/products-list/product-list-nav.jsx
+++ src/components/products-list/product-list-nav.jsx
@@ -1,45 +1,31 @@
 import React, {useContext} from "react";
-import {Link, useRouteMatch} from 'react-router-dom';
+import {Link, useRouteMatch, } from 'react-router-dom';
 import StateContext from "../../context";
 
-import {Accordion, Card, Button} from "react-bootstrap";
+import {DropdownButton, Dropdown} from "react-bootstrap";
 
 const ProductListNavElem = ({title}) => {
     const {url} = useRouteMatch();
     return (
-        <div className="col-12 col-md-6 col-lg-4">
+        <Dropdown.Item as={"div"}>
             <Link to={`${url}/${title}`}
-                  replace
-                  className="btn btn-info mb-3 w-100">
+                  replace>
                 {title}
             </Link>
-        </div>
+        </Dropdown.Item>
     )
 }
 
 const ProductListNav = () => {
     const {products} = useContext(StateContext);
     return (
-        <Accordion defaultActiveKey="0">
-            <Card>
-                <Card.Header>
-                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                        Категории
-                    </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                    <Card.Body>
-                        <div className="row productList-nav align-content-start">
-                            {
-                                Object.keys(products).map((el, idx) => {
-                                    return <ProductListNavElem title={el} key={idx}/>
-                                })
-                            }
-                        </div>
-                    </Card.Body>
-                </Accordion.Collapse>
-            </Card>
-        </Accordion>
+        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+            {
+                Object.keys(products).map((el, idx) => {
+                    return <ProductListNavElem title={el} key={idx}/>
+                })
+            }
+        </DropdownButton>
 
     )
 }
