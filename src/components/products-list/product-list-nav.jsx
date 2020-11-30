a3a1e617c4e459503abcce91b2efd4eced6ed385
Oleg <talrash666@gmail.com>
2020-11-30 16:07:40 +0300

    change products.json

--- src/components/products-list/product-list-nav.jsx
+++ src/components/products-list/product-list-nav.jsx
@@ -1,31 +1,45 @@
 import React, {useContext} from "react";
-import {Link, useRouteMatch, } from 'react-router-dom';
+import {Link, useRouteMatch} from 'react-router-dom';
 import StateContext from "../../context";
 
-import {DropdownButton, Dropdown} from "react-bootstrap";
+import {Accordion, Card, Button} from "react-bootstrap";
 
 const ProductListNavElem = ({title}) => {
     const {url} = useRouteMatch();
     return (
-        <Dropdown.Item as={"div"}>
+        <div className="col-12 col-md-6 col-lg-4">
             <Link to={`${url}/${title}`}
-                  replace>
+                  replace
+                  className="btn btn-info mb-3 w-100">
                 {title}
             </Link>
-        </Dropdown.Item>
+        </div>
     )
 }
 
 const ProductListNav = () => {
     const {products} = useContext(StateContext);
     return (
-        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-            {
-                Object.keys(products).map((el, idx) => {
-                    return <ProductListNavElem title={el} key={idx}/>
-                })
-            }
-        </DropdownButton>
+        <Accordion defaultActiveKey="0">
+            <Card>
+                <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                        Категории
+                    </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        <div className="row productList-nav align-content-start">
+                            {
+                                Object.keys(products).map((el, idx) => {
+                                    return <ProductListNavElem title={el} key={idx}/>
+                                })
+                            }
+                        </div>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
 
     )
 }
